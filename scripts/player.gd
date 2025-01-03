b4470839fcd3e1cfe8c07eb89e577f89b551e8ea
Th9Fan <git@twinion.top>
2025-01-02 22:17:37 -0800

    kill zone and time

--- scripts/player.gd
+++ scripts/player.gd
@@ -11,15 +11,27 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
+	if Input.is_action_just_pressed("slow0.5"):
+		Engine.time_scale = 0.5
+		
+	if Input.is_action_just_pressed("slow0.3"):
+		Engine.time_scale = 0.3
+		
+	if Input.is_action_just_pressed("slow0.25"):
+		Engine.time_scale = 0.25
+		
+	if Input.is_action_just_pressed("normal"):
+		Engine.time_scale = 1
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
 
 	move_and_slide()
